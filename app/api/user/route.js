4d638dee482df8408cb580ff27406fae2d2f97e7
Sohum <sohumshah2@gmail.com>
2024-01-19 21:34:21 +1100

    connected to mongodb; store user name and email when a new user signs in

---
+++ app/api/user/route.js
@@ -0,0 +1,11 @@
+// import User from "@/models/user.ts";
+import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../lib/mongodb";
+import User from "../../../models/user.ts";
+
+export async function POST(request) {
+  const { name, email } = await request.json();
+  await connectMongoDB();
+  await User.create({ name, email });
+  return NextResponse.json({ message: "User Registered" }, { status: 201 });
+}
